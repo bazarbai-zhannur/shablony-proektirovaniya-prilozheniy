de1ec5f9627fdf31ec75b989e4c791675d7f1d0b
bazarbai-zhannur <mrsmask092@gmail.com>
2026-02-08 17:49:13 +0500

    Create Order.java

---
+++ homeworks/hm3/single_responsibility_principle/Order.java
@@ -0,0 +1,17 @@
+public class Order {
+    private int quantity;
+    private double price;
+
+    public Order(int quantity, double price) {
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
