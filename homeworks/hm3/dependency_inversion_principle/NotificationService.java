7e09697633ebf410a466e9f1b9a1b869abd7befa
bazarbai-zhannur <mrsmask092@gmail.com>
2026-02-08 17:55:16 +0500

    Create NotificationService.java

---
+++ homeworks/hm3/dependency_inversion_principle/NotificationService.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class NotificationService {
+    private List<NotificationChannel> channels;
+
+    public NotificationService(List<NotificationChannel> channels) {
+        this.channels = channels;
+    }
+
+    public void notifyAll(String message) {
+        for (NotificationChannel channel : channels) {
+            channel.send(message);
+        }
+    }
+}
